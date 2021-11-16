51686c9050f212014cbb92989076dc2cd6786e0e
Marko Klemetti <marko.klemetti@gmail.com>
2021-11-16 16:30:38 +0200

    refactor(action): refactor script code

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-async function run() {
+(async () => {
   try {
-    const prUrl = github.context.payload.issue.pull_request.url
-    const commentsUrl = github.context.payload.issue.comments_url
+    const { comments_url, pull_request: { url } } = github.context.payload.issue;
+
     const githubToken = core.getInput('token');
     const octokit = github.getOctokit(githubToken);
-    const prDetails = await octokit.request(`GET ${prUrl}`)
+
+    const prDetails = await octokit.request(`GET ${prUrl}`);
+
     const status = prDetails.status
     const headRef = prDetails.data.head.ref
-    console.log("head ref")
-    console.log(headRef)
+
     const baseRef = prDetails.data.base.ref
-    console.log("base ref")
-    console.log(baseRef)
+
     core.setOutput('base_ref', baseRef);
     core.setOutput('head_ref', headRef);
-  }
-  catch (error) {
+  } catch (error) {
     core.setFailed(error.message);
   }
-}
-
-run()
+})();
